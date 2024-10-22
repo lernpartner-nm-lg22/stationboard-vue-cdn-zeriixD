d480ae48a4087cef74d4797b0525de6da1836489
zeriixD <143174776+zeriixD@users.noreply.github.com>
2024-10-22 13:56:47 +0200

    super

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,31 @@
-"use strict"
+"use strict";
 // opendata api URL
-const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
 
-const getData = async () => {};
+const stationboard = {
+    data() {
+        return {
+            station: "",
+            stationName: "",
+            trains: [],
+        };
+    },
+    methods: {
+    async loadConnections() {
+        if (this.station === "") return;
+        const url = `https://transport.opendata.ch/v1/stationboard?station=${this.station}`;
+        const response = await fetch(url);
+        const data = await response.json();
 
-const displayData = (data) => {};
+        this.stationName = data.station.name;
+        this.trains = data.stationboard;
+    },
+    timestampToTime(timestamp) {
+        const date = new Date(timestamp);
+       return date.toLocaleTimeString("de-CH", { hour: "2-digit", minute: "2-digit" });
+
+    },
+  }
+};
+
+const app = Vue.createApp(stationboard);
+app.mount("#app");
